094862091177b27801faad54186123ffa3795bfc
sm-lucas <133544611+sm-lucas@users.noreply.github.com>
2024-01-11 22:13:24 -0300

    Definindo string
    Definindo string para as URLs que irei utilizar

--- PrimeiraAPI/PrimeiraAPI/Program.cs
+++ PrimeiraAPI/PrimeiraAPI/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Teste para iniciar produção");
+﻿using System;
+using System.Net.Http;
+
+namespace PrimeiraAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // url das marcas dos veiculos 
+            string urlMarca = "https://parallelum.com.br/fipe/api/v1/carros/marcas";
+            //url dos modelo de veiculos 
+            string urlModelo = "https://parallelum.com.br/fipe/api/v1/carros/marcas/59/modelos";
+
+            // a seguir devera criar um Cliente http, chamar a API das marcas
+
+
+        }
+    }
+}
