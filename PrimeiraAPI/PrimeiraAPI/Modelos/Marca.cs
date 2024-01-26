1b046fcbdb7e4865c3665135fce40a88e777f554
engDaniloOS <36059157+engDaniloOS@users.noreply.github.com>
2024-01-25 22:42:34 -0300

    Observacoes para seguir

---
+++ PrimeiraAPI/PrimeiraAPI/Modelos/Marca.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PrimeiraAPI.Modelos
+{
+    public class Marca
+    {
+        [JsonPropertyName("Nome")]
+        public String Nome { get; set; }
+
+        //Adicionar as outras propriedades, conforme JSON de retorno
+    }
+}
--- PrimeiraAPI/PrimeiraAPI/Program.cs
+++ PrimeiraAPI/PrimeiraAPI/Program.cs
@@ -21,21 +21,27 @@ namespace PrimeiraAPI
             // chama a API marcas
             var responseMarcas = client.GetAsync("https://parallelum.com.br/fipe/api/v1/carros/marcas").Result;
 
-            //obtém os dados da resposta
-            var marcas = responseMarcas.Content.ReadAsStringAsync().Result;
+            //obtém os dados da resposta COMO UMA STRING
+            //E só obtem caso o retorno seja 200 (OK)
+            var json = responseMarcas.Content.ReadAsStringAsync().Result;
+
+            ///////////////////////////////////////////////////////////////////////////
+            ///Converta o JSON em uma lista do objeto marca (mas crie todas as propriedades na marca primeiro. Deixei uma já criada
+            ///Depois você itera, e trata o resultado
+            ///O conteúdo de retorno de uma API geralmente é um JSON (string). E no código você converte para objetos antes de seguir com a lógica
 
             //Percorre as marcas. Fazendo a leitura
-            foreach (var marca in marcas)
-            {
+            //foreach (var marca in marcas)
+            //{
                 // Cria o nome do arquivo
-                string nomeArquivo = marca.Nome.ToLower() + ".txt";
+                //string nomeArquivo = marca.Nome.ToLower() + ".txt";
 
                 //Cria um arquivo
-                StreamWriter writer = new StreamWriter(nomeArquivo);
+                //StreamWriter writer = new StreamWriter(nomeArquivo);
 
                 //obs:Continuar verificando erro em Nome na linha 31 
 
-            }
+            //}
         }
     }
 }
